3ba5bf523bb1a4a0f8b493c61e02f264ea974d00
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-14 02:32:47 +0100

    Household class created. Main program updated. Application can be run.

---
+++ MedlandG/Household.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedlandG
+{
+    public class Household
+    {
+        public List<Person> Occupants { get; private set; }
+        public List<IHouseholdArea> Areas { get; private set; }
+
+        public Household()
+        {
+            Occupants = new List<Person>();
+            Areas = new List<IHouseholdArea>();
+        }
+
+        public void AddOccupant(Person person)
+        {
+            Occupants.Add(person);
+        }
+
+        public void AddArea(IHouseholdArea area)
+        {
+            Areas.Add(area);
+        }
+
+        public void DescribeHousehold()
+        {
+            Console.WriteLine("Household Description:");
+
+            Console.WriteLine("Occupants:");
+            foreach(var occupant in Occupants)
+            {
+                occupant.Describe();
+            }
+
+            Console.WriteLine("Areas:");
+            foreach(var area in Areas)
+            {
+                area.DescribeArea();
+            }
+        }
+    }
+}
--- MedlandG/Program.cs
+++ MedlandG/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create household
+            Household household = new Household();
+
+            // Add occupant
+            household.AddOccupant(new Parent("Colin", 61));
+            household.AddOccupant(new Parent("Aly", 58));
+            household.AddOccupant(new Son("Kieran", 29));
+
+            // Add rooms and areas
+            household.AddArea(new Room("Parent's Bedroom", true));
+            household.AddArea(new Room("Son's Bedroom", true));
+            household.AddArea(new Room("Vacant Bedroom 1", false));
+            household.AddArea(new Room("Vacant Bedroom 2", false));
+            household.AddArea(new Bathroom(true)); // En-suite bathroom
+            household.AddArea(new Bathroom(false)); // Shared bathroom
+            household.AddArea(new Room("Toilet", false));
+            household.AddArea(new Room("Lounge", false));
+            household.AddArea(new Room("Kitchen", false));
+            household.AddArea(new Room("Utility Room", false));
+            household.AddArea(new Room("Study", false));
+            household.AddArea(new Room("Dining Room", false));
+
+            // Add garden with features
+            Garden garden = new Garden();
+            garden.AddFeature("Shed");
+            garden.AddFeature("Patio");
+            garden.AddFeature("Greenhouse");
+            garden.AddFeature("Sun Loungers");
+            garden.AddFeature("Fruit Plants");
+
+            household.AddArea(garden);
+
+            // Describe household
+            household.DescribeHousehold();
         }
     }
 }
